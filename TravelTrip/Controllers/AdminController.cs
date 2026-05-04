713edadbc90e882e81e1499681a9c98c6429379b
semraincedayi <semraincedayi@hotmail.com>
2026-05-04 20:07:25 +0300

    Admin paneli blog listeleme sayfası

---
+++ TravelTrip/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Class;
+
+namespace TravelTrip.Controllers
+{
+    public class AdminController : Controller
+    {
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var degerler = context.Blogs.ToList();
+            return View(degerler);
+        }
+        
+    }
+}
