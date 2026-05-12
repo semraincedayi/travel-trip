f32ceb6efb57b82e5bf1044d8d846f59fd89e4e0
semraincedayi <semraincedayi@hotmail.com>
2026-05-12 14:52:01 +0300

    Admin giris sayfasi için template entegre edildi

---
+++ TravelTrip/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelTrip.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
