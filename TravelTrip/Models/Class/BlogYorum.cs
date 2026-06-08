3a1206af0aa069c6e667259ee8e7b29736eca798
semraincedayi <semraincedayi@hotmail.com>
2026-06-08 12:26:34 +0300

    feat: Blog detay sayfasına son yorumlar modülünün entegre edilmesi

--- TravelTrip/Models/Class/BlogYorum.cs
+++ TravelTrip/Models/Class/BlogYorum.cs
@@ -10,7 +10,8 @@ namespace TravelTrip.Models.Class
     {
       public IEnumerable<TravelTrip.ViewModels.BlogSumViewModel> DegerBlog { get; set; }
       public IEnumerable<Blog> Deger1 { get; set; }
-      public IEnumerable<Yorumlar> Deger2 { get; set; }
+        public IEnumerable<Yorumlar> Deger2 { get; set; }
       public IEnumerable<Blog> Deger3 { get; set; }
+        public IEnumerable<Yorumlar> Deger4 { get; set; }
     }
 }
