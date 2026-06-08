c39ac0a70f760711c3e98222d27effaec18ae2b2
semraincedayi <semraincedayi@hotmail.com>
2026-06-08 12:18:03 +0300

    feat: İletişim sayfası ana görünüm ve routing altyapısı eklendi

---
+++ TravelTrip/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Class;
+
+namespace TravelTrip.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        Context _context = new Context();
+        public ActionResult ContactInf()
+        {
+            var degerler = _context.Iletisims.ToList();
+            return View(degerler);
+            
+        }
+        
+    }
+}
