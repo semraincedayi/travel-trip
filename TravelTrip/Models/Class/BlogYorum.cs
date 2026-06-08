9ce3eb6907eb4fec616f1fa38866dce02ba1b15e
semraincedayi <semraincedayi@hotmail.com>
2026-06-08 12:05:50 +0300

    feat:sayfalama ve blog listelemede açıklamaya 150 karakter sınırı eklendi.

--- TravelTrip/Models/Class/BlogYorum.cs
+++ TravelTrip/Models/Class/BlogYorum.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TravelTrip.ViewModels;
 
 namespace TravelTrip.Models.Class
 {
     public class BlogYorum
     {
+      public IEnumerable<TravelTrip.ViewModels.BlogSumViewModel> DegerBlog { get; set; }
       public IEnumerable<Blog> Deger1 { get; set; }
       public IEnumerable<Yorumlar> Deger2 { get; set; }
       public IEnumerable<Blog> Deger3 { get; set; }
